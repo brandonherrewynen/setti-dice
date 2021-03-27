f171e6f02d1d08d69fe7c7175c5c96edd57d7eda
Brandon Herrewynen <b.herrewynen@gmail.com>
2021-03-27 12:08:43 -0400

    check

--- index.js
+++ index.js
@@ -2,25 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const storage = require('node-persist');
 const path = require('path');
 const dice = require('./dice');
 app.use(express.static(path.join(__dirname, '/public')));
-
-async function initStorage() {
-    await storage.init({
-        dir: '/logs/',
-        stringify: JSON.stringify,
-        parse: JSON.parse,
-        encoding: 'utf8',
-        logging: false,  // can also be custom logging function
-        ttl: false, // ttl* [NEW], can be true for 24h default or a number in MILLISECONDS or a valid Javascript Date object
-        expiredInterval: 2 * 60 * 1000, // every 2 minutes the process will clean-up the expired cache
-        // in some cases, you (or some other service) might add non-valid storage files to your
-        // storage dir, i.e. Google Drive, make this true if you'd like to ignore these files and not throw an error
-        forgiveParseErrors: false
-    });
-}
 
 var i = dice.DiceRound();
 
@@ -28,7 +12,6 @@ app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
-initStorage();
 io.sockets.on('connection', function(socket) {
     socket.on('username', function(username) {
         socket.username = username;
@@ -43,6 +26,9 @@ io.sockets.on('connection', function(socket) {
         //     console.log(result);
         // })
     });
+    socket.on('error', (error) => {
+        io.emit('chat_message', "coonection error" + error);
+    })
 });
 
 const server = http.listen(8080, function() {
